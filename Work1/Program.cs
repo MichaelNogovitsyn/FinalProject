5d60cf76354d64b4366d3023fff8fd9007f79e8f
mexatron <mexatron>
2022-12-29 21:14:15 +0200

    Metod CountSynbolArray done

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,3 +1,29 @@
-﻿ 
- string[] InitArray={"Hello", "2", "world",":-)"};
-System.Console.WriteLine(InitArray[0]);
+﻿
+string[] InitArray = { "Hello", "2", "world", ":-)" };
+
+int CountSynbolArray(string[] array, int numSymb)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numSymb)
+        count++;
+    }
+    return count;
+}
+
+void defineArray(string[] array, string[] newArray, int numSymb)
+{
+for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numSymb)
+        {
+            newArray[j] = InitArray[i];
+            j++;
+        }
+    }
+}
+
+int CountArr = CountSynbolArray(InitArray,3);
+
+//System.Console.WriteLine(CountArr);
